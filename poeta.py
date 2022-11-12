629addee18752f427f10b75702d26358e3aa38f9
Iñaki Silanes <isilanes@gmail.com>
2022-11-12 12:43:59 +0100

    Improve configuration.

--- poeta.py
+++ poeta.py
@@ -6,75 +6,75 @@ from typing import Optional
 STRINGS = {
     1: {
         "str": "hablando con",
-        "next": [2],
+        "next": [2, 4, 6, 7, 9, 12, 13, 17],
     },
     2: {
         "str": "inmigrantes",
-        "next": [],
+        "next": [3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18],
     },
     3: {
         "str": "agonizaron durante",
-        "next": [4],
+        "next": [4, 13],
     },
     4: {
         "str": "las revelaciones",
-        "next": [5],
+        "next": [3, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18],
     },
     5: {
         "str": "me sorprende ver",
-        "next": [6],
+        "next": [1, 2, 4, 6, 8, 9, 12, 13, 17],
     },
     6: {
         "str": "un bosque para",
-        "next": [7],
+        "next": [2, 4, 7, 8, 9, 12, 13, 17],
     },
     7: {
         "str": "nosotros",
-        "next": [8],
+        "next": [1, 2, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
     },
     8: {
         "str": "cuando tu",
-        "next": [9],
+        "next": [9, 12],
     },
     9: {
         "str": "belleza especial",
-        "next": [10],
+        "next": [1, 2, 3, 4, 5, 6, 7, 8, 11, 12, 13, 14, 15, 16, 17, 18],
     },
     10: {
         "str": "e incluso la intuición",
-        "next": [11],
+        "next": [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18],
     },
     11: {
         "str": "apabulla con su",
-        "next": [12],
+        "next": [9, 12],
     },
     12: {
         "str": "corte y",
-        "next": [13],
+        "next": [3, 7, 9, 13, 14, 15, 16, 17, 18],
     },
     13: {
-        "str": "hervidero de ideas",
-        "next": [14],
+        "str": "hervideros de ideas",
+        "next": [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18],
     },
     14: {
         "str": "ahora es mi momento",
-        "next": [15],
+        "next": [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 15, 16, 17, 18],
     },
     15: {
         "str": "le voy a poner",
-        "next": [16],
+        "next": [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 16, 17, 18],
     },
     16: {
         "str": "piden más",
-        "next": [17],
+        "next": [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 17, 18],
     },
     17: {
         "str": "botellas de cristal",
-        "next": [18],
+        "next": [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 18],
     },
     18: {
         "str": "sean normales",
-        "next": [],
+        "next": [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17],
     },
 }
 
